a61691d38fd4781dbd83559415461c59dabbdff7
sajidul-rilo <msalam@rilogroup.com>
2025-01-21 14:47:24 +0600

    [Done]: Lesson Schema

---
+++ src/app/lesson/page.tsx
@@ -0,0 +1,13 @@
+import Lesson from "@/features/lessons/components/Lesson";
+import React from "react";
+
+const LessonPage = () => {
+  return (
+    <div>
+      Lesson page
+      <Lesson />
+    </div>
+  );
+};
+
+export default LessonPage;
