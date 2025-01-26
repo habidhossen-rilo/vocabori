9f1b9482d09bc84bbdaf0ec3e71ee9f5a94da3f0
mfmakib-rilo <mfmakib@rilogroup.com>
2025-01-26 10:03:52 +0600

    session provider resolved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const session = await getServerSession(authOptions);
+  console.log("session from layout---->", session);
   return (
     <html lang="en">
       <body
