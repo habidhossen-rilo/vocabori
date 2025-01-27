ef6b4452c10461f36e8cd29099ed0f3328b9e2d3
mfmakib-rilo <mfmakib@rilogroup.com>
2025-01-27 14:28:11 +0600

    updated ui

--- src/features/lessons/components/SingleLesson/SingleLesson.tsx
+++ src/features/lessons/components/SingleLesson/SingleLesson.tsx
@@ -1,12 +1,16 @@
 import { Button } from "@/components/ui/button";
 import { LessonProps } from "../../types/lesson";
-import style from "./singleLesson.module.css";
 import Link from "next/link";
+import { BookCheck } from "lucide-react";
+import style from "../../styles/singleLesson.module.css";
 
 const SingleLesson = ({ lesson }: LessonProps) => {
   return (
     <div className={style.lessonContainer}>
-      <p>{lesson.lesson_name}</p>
+      <p className={style.lessonName}>
+        <BookCheck />
+        {lesson.lesson_name}
+      </p>
       <Link href={`/lessons/${lesson._id}`}>
         <Button variant="ghost">Learn</Button>
       </Link>
