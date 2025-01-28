c6aca27a8805644c49d93e52b2497c70da719269
mfmakib-rilo <mfmakib@rilogroup.com>
2025-01-28 12:12:42 +0600

    [Done]:Nav Responsive

--- src/components/shared/Navbar/NavLink/NavLink.tsx
+++ src/components/shared/Navbar/NavLink/NavLink.tsx
@@ -5,13 +5,17 @@ import styles from "./navLink.module.css";
 import { usePathname } from "next/navigation";
 import { NavLinkProps } from "@/types/navbar";
 
-const NavLink = ({ item }: NavLinkProps) => {
+const NavLink = ({
+  item,
+  onClick,
+}: NavLinkProps & { onClick?: () => void }) => {
   const pathName = usePathname();
 
   return (
     <Link
       href={item.path}
       className={`${styles.navLink} ${pathName === item.path && styles.active}`}
+      onClick={onClick}
     >
       {item.title}
     </Link>
