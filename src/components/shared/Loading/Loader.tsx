53ab187cfc05acfcba4fb897d79b80d089f8cfc0
sajidul-rilo <msalam@rilogroup.com>
2025-01-28 12:21:39 +0600

    [Done]: Loading added on page

--- src/components/shared/Loading/Loader.tsx
+++ src/components/shared/Loading/Loader.tsx
@@ -1,7 +1,7 @@
 import styles from "./loading.module.css";
 const Loader = () => {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex h-[100vh] items-center justify-center">
       <div className={styles.customLoader}></div>
     </div>
   );
